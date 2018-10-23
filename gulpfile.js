e5e1a4430f457e6640ab2d28811c80cbb48f2a1e
ernaneaugusto <ernaneatcipriano@gmail.com>
2018-10-23 20:45:45 -0300

    Inserido gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var plumber = require('gulp-plumber');
 
 var pastaSCSS = ["./scss/*.scss"];
 var pastaCSS = "./css/";
 
 gulp.task("sass", function () {
     return gulp.src(pastaSCSS)
+        .pipe(plumber())
         .pipe(sass())
         .pipe(gulp.dest(pastaCSS));
 });
